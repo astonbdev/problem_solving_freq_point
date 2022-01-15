0c8f58a90ceeb4b8c7c4001119037dbb0a0c891f
Brian Aston <astonb.dev@gmail.com>
2022-01-14 16:20:12 -0800

    Create can-construct-word function. Fixed test html

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,31 @@
-// add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+/**takes in two strings, one word and one a series of letters. 
+ * Compares the contents of the letters with the word 
+ * to see if the word can be constructed from the given letter 
+ * returns boolean
+ * assumes both strings contain only lowercase letters */
+function canConstructWord(word, letters) {
+    let wordCounts = frequencyCounter(word.split(""));
+    let letterCounts = frequencyCounter(letters.split(""));
+
+    for(let letter of word){
+        if(!(letterCounts[letter] >= wordCounts[letter])){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+/** standard frequency counter takes in array, returns key value pairs with value as counts */
+
+function frequencyCounter(arr){
+    let freqs = {};
+
+    for(let val of arr){
+        let valCount = freqs[val] || 0;
+
+        freqs[val] = valCount + 1;
+    }
+
+    return freqs;
 }
