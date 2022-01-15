f53aa78bee3c3eed4c78a9456e17b78f63361f45
Peter Loft <peterloft@Peters-MacBook-Pro.local>
2022-01-14 18:40:13 -0600

    Completed function for average-pairs

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,24 @@
-// add whatever parameters you deem necessary
-function averagePair() {
+"use strict";
+
+/**Accepts a sorted array of numbers and a target average.  
+ * averages the sum of two value pairs and
+ * returns true if equivalent to the target.
+*/
+function averagePair(nums, trgtAverage) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let average = (nums[left] + nums[right]) / 2;
+        
+        if (trgtAverage === average) {
+            return true;
+        } else if (trgtAverage > average) {
+            left++;
+        } else {
+            right--;
+        }
+
+    }
+    return false;
 }
